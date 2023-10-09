7ff1bbd18b3df252cba24c4fa9fc32ea6f8c0cfb
Samba@X13 <santos.diogo.21@outlook.com>
2023-10-09 12:22:49 +0100

    BANK COM GIGALOCK

--- guiao3/src/Bank.java
+++ guiao3/src/Bank.java
@@ -81,50 +81,163 @@ class Bank {
     }
 
     // account balance; 0 if no such account
-    public int balance(int id) {
-        Account c = map.get(id);
+    public int balance(int id) 
+    {
+        Account c;    
+        GIGALOCK.lock();
+        try
+        {
+            c = map.get(id);
+        }
+        finally
+        {
+            GIGALOCK.unlock();
+        }
+
         if (c == null)
             return 0;
-        return c.balance();
+
+        c.l.lock();
+        try
+        {
+            return c.balance();
+        }
+        finally
+        {
+            c.l.unlock();
+        }
     }
 
     // deposit; fails if no such account
-    public boolean deposit(int id, int value) {
-        Account c = map.get(id);
+    public boolean deposit(int id, int value)
+    {
+        Account c;
+        GIGALOCK.lock();
+        try
+        {
+            c = map.get(id);
+        }
+        finally
+        {
+            GIGALOCK.unlock();
+        }
+
         if (c == null)
             return false;
-        return c.deposit(value);
+
+        c.l.lock();
+        try
+        {
+            return c.deposit(value);
+        }
+        finally
+        {
+            c.l.unlock();
+        }
     }
 
     // withdraw; fails if no such account or insufficient balance
-    public boolean withdraw(int id, int value) {
-        Account c = map.get(id);
+    public boolean withdraw(int id, int value)
+    {
+        Account c;
+        GIGALOCK.lock();
+        try
+        {
+            c = map.get(id);
+        }
+        finally
+        {
+            GIGALOCK.unlock();
+        }
+
         if (c == null)
             return false;
-        return c.withdraw(value);
+
+        c.l.lock();
+        try
+        {
+            return c.withdraw(value);
+        }
+        finally
+        {
+            c.l.unlock();
+        }
     }
 
     // transfer value between accounts;
     // fails if either account does not exist or insufficient balance
-    public boolean transfer(int from, int to, int value) {
+    public boolean transfer(int from, int to, int value)
+    {
         Account cfrom, cto;
-        cfrom = map.get(from);
-        cto = map.get(to);
+        GIGALOCK.lock();
+        try
+        {
+            cfrom = map.get(from);
+            cto = map.get(to);
+        }
+        finally
+        {
+            GIGALOCK.unlock();
+        }
         if (cfrom == null || cto ==  null)
             return false;
-        return cfrom.withdraw(value) && cto.deposit(value);
+
+        if (cto> cfrom)
+        {
+            cfrom.l.lock();
+            cto.l.lock();
+        }
+        else
+        {
+            cto.l.lock();
+            cfrom.l.lock();
+        }
+        try
+        {
+            try
+            {
+                if (!cfrom.withdraw(value))
+                    return false;
+            }
+            finally
+            {
+                cfrom.l.unlock();
+            }
+            return cto.deposit(value);
+        }
+        finally
+        {
+            cto.l.unlock();
+        }
     }
 
     // sum of balances in set of accounts; 0 if some does not exist
     public int totalBalance(int[] ids) {
         int total = 0;
-        for (int i : ids) {
-            Account c = map.get(i);
-            if (c == null)
-                return 0;
-            total += c.balance();
+        GIGALOCK.lock();
+        try
+        {
+            for (int i : ids)
+            {
+                Account c = map.get(i);
+                if (c == null)
+                    return 0;
+
+                c.l.lock();
+                try
+                {    
+                    total += c.balance();
+                }
+                finally
+                {    
+                    c.l.unlock();
+                }
+            }
+        }
+        finally
+        {
+            GIGALOCK.unlock();
         }
         return total;
   }
-
 }
