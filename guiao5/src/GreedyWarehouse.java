50d64b3ae1ba2373924bb19b5ffaaabc6f5092b3
Samba@X13 <santos.diogo.21@outlook.com>
2023-10-23 12:18:14 +0100

    Cenas do guião 5

---
+++ guiao5/src/GreedyWarehouse.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+class GreedyWarehouse 
+{
+  	private Map<String, Product> map =  new HashMap<String, Product>();
+	public ReentrantLock l= new ReentrantLock();
+   
+	private class Product
+	{
+		Condition cond= new Condition(l);
+		int quantity = 0;
+	}
+
+	private Product get(String item) 
+	{
+		Product p = map.get(item);
+		if (p != null) 
+			return p;
+		p = new Product();
+		map.put(item, p);
+		return p;
+	}
+
+	public void supply(String item, int quantity)
+	{
+		Product p = get(item);
+		p.quantity += quantity;
+	}
+
+	// Errado se faltar algum produto...
+	public void consume(Set<String> items)
+	{
+		l.lock();
+		try
+		{
+			for (Stirng s: items)
+			{
+				Product p= get(s);
+				if (p.quantity== 0)
+					p.cond.await();
+				p.quantity--;
+			}
+		}
+		finally
+		{
+			l.unlock();
+		}
+	}
+}
